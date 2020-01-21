fdfc767442f66cd63a3b1ea489976a257e2487cc
Joni Baboci <dbaboci@gmail.com>
2020-01-21 23:23:52 +0100

    Customizing original script
    Removed what I don't think I will need
    Commented what might be interesting

--- fedora_new_install.sh
+++ fedora_new_install.sh
@@ -1,5 +1,5 @@
 ###
-Original Script from: https://git.furworks.de/tobias/fedora-install/src/branch/master/install.sh - modified here for my needs
+#Original Script from: https://git.furworks.de/tobias/fedora-install/src/branch/master/install.sh - modified here for my needs
 ###
 
 #!/bin/bash
@@ -77,112 +77,58 @@ sudo dnf distro-sync -y
 sudo dnf install \
 -y \
 arc-theme `#A more comfortable GTK/Gnome-Shell Theme` \
-blender `#3D Software Powerhouse` \
 breeze-cursor-theme `#A more comfortable Cursor Theme from KDE` \
 calibre `#Ebook management` \
 chrome-gnome-shell `#Gnome <> Browser Integration for the gnome plugins website` \
-chromium-vaapi `#Comes with hardware acceleration and all Codecs` \
-darktable `#Easy RAW Editor` \
-evolution-spamassassin `#Helps you deal with spam in Evolution` \
 exfat-utils `#Allows managing exfat (android sd cards and co)` \
 ffmpeg `#Adds Codec Support to Firefox, and in general` \
-file-roller-nautilus `#More Archives supported in nautilus` \
-filezilla `#S/FTP Access` \
-fuse-exfat `#Allows mounting exfat` \
-fuse-sshfs `#Allows mounting servers via sshfs` \
-gimp `#The Image Editing Powerhouse - and its plugins` \
-gimp-data-extras \
-gimp-dbp \
-gimp-dds-plugin \
-gimp-elsamuko \
-gimp-focusblur-plugin \
-gimp-fourier-plugin \
-gimpfx-foundry.noarch \
-gimp-gap \
-gimp-high-pass-filter \
-gimp-layer-via-copy-cut \
-gimp-lensfun \
-gimp-lqr-plugin \
-gimp-luminosity-masks \
-gimp-paint-studio \
-gimp-resynthesizer \
-gimp-save-for-web \
-gimp-wavelet-decompose \
-gimp-wavelet-denoise-plugin \
+###
+#gimp `#The Image Editing Powerhouse - and its plugins` \
+#gimp-data-extras \
+#gimp-dbp \
+#gimp-dds-plugin \
+#gimp-elsamuko \
+#gimp-focusblur-plugin \
+#gimp-fourier-plugin \
+#gimpfx-foundry.noarch \
+#gimp-gap \
+#gimp-high-pass-filter \
+#gimp-layer-via-copy-cut \
+#gimp-lensfun \
+#gimp-lqr-plugin \
+#gimp-luminosity-masks \
+#gimp-paint-studio \
+#gimp-resynthesizer \
+#gimp-save-for-web \
+#gimp-wavelet-decompose \
+#gimp-wavelet-denoise-plugin \
+###
 git `#VCS done right` \
-gmic-gimp \
 gnome-shell-extension-dash-to-dock `#dash for gnome` \
 gnome-shell-extension-topicons-plus `#Notification Icons for gnome` \
 gnome-shell-extension-user-theme `#Enables theming the gnome shell` \
 gnome-tweak-tool `#Your central place to make gnome like you want` \
-GREYCstoration-gimp \
-gtkhash-nautilus `#To get a file has via gui` \
-gvfs-fuse `#gnome<>fuse` \
-gvfs-mtp `#gnome<>android` \
-gvfs-nfs `#gnome<>ntfs` \
-gvfs-smb `#gnome<>samba` \
-htop `#Cli process monitor` \
-inkscape  `#Working with .svg files` \
-kdenlive  `#Advanced Video Editor` \
 keepassxc  `#Password Manager` \
-krita  `#Painting done right keep in mind mypaint and gimp cant work together in current upstream versions - yet` \
-libreoffice-gallery-vrt-network-equipment `#Network Icon Preset for LibreOffice` \
-lm_sensors `#Show your systems Temparature` \
 'mozilla-fira-*' `#A nice font family` \
 mpv `#The best media player (with simple gui)` \
 mumble `#Talk with your friends` \
-nautilus-extensions `#What it says on the tin` \
-nautilus-image-converter \
-nautilus-search-tool \
-NetworkManager-openvpn-gnome `#To enforce that its possible to import .ovpn files in the settings` \
-openshot `#Simple Video Editor` \
-openssh-askpass `#Base Lib to let applications request ssh pass via gui` \
 p7zip `#Archives` \
 p7zip-plugins `#Even more of them` \
 papirus-icon-theme `#A quite nice icon theme` \
-pv `#pipe viewer - see what happens between the | with output | pv | receiver ` \
 python3-devel `#Python Development Gear` \
 python3-neovim `#Python Neovim Libs` \
-rawtherapee `#Professional RAW Editor` \
-spamassassin `#Dep to make sure it is locally installed for Evolution` \
 telegram-desktop `#Chatting, with newer openssl and qt base!` \
-tilix `#The best terminal manager i know of` \
-tilix-nautilus `#Adds right click open in tilix to nautilus` \
 transmission `#Torrent Client` \
 tuned `#Tuned can optimize your performance according to metrics. tuned-adm profile powersave can help you on laptops, alot` \
 unar `#free rar decompression` \
-vagrant `#Virtual Machine management and autodeployment` \
-vagrant-libvirt `#integration with libvirt` \
-virt-manager `#A gui to manage virtual machines` \
-wavemon `#a cli wifi status tool` \
 youtube-dl `#Allows you to download and save youtube videos but also to open their links by dragging them into mpv!` \
-ansible `#Awesome to manage multiple machines or define states for systems` \
 adobe-source-code-pro-fonts `#The most beautiful monospace font around` \
 borgbackup `#If you need backups, this is your tool for it` \
 gitg `#a gui for git, a little slow on larger repos sadly` \
-iotop  `#disk usage cli monitor` \
-meld `#Quick Diff Tool` \
-nano `#Because pressing i is too hard sometimes` \
-neovim `#the better vim` \
 nethogs `#Whats using all your traffic? Now you know!` \
 nload `#Network Load Monitor` \
-tig `#cli git tool` \
-vim-enhanced `#full vim` \
 zsh `#Best shell` \
 zsh-syntax-highlighting `#Now with syntax highlighting` \
-cantata `#A beautiful mpd control` \
-caddy `#A quick webserver that can be used to share a directory with others in <10 seconds` \
-cockpit `#A An awesome local and remote management tool` \
-cockpit-bridge \
-fortune-mod `#Inspiring Quotes` \
-hexchat `#Irc Client` \
-libguestfs-tools `#Resize Vm Images and convert them` \
-ncdu `#Directory listing CLI tool. For a gui version take a look at "baobab"` \
-nextcloud-client `#Nextcloud Integration for Fedora` \
-nextcloud-client-nautilus `#Also for the File Manager, shows you file status` \
-sqlite-analyzer `#If you work with sqlite databases` \
-sqlitebrowser `#These two help alot` \
-syncthing-gtk `#Syncing evolved - to use the local only mode open up the ports with firewall-cmd --add-port=port/tcp --permanent && firewall-cmd --reload` 
 
 ###
 # Remove some un-needed stuff
@@ -192,7 +138,6 @@ sudo dnf remove \
 -y \
 gnome-shell-extension-background-logo `#Tasteful but nah` \
 totem `#With mpv installed totem became a little useless` \
-chromium `#Using Chromium resets chromium-vaapi so remove it if installed, userprofiles will be kept and can be used in -vaapi` \
 flowblade `#Sadly has really outdated mlt dependencies`
 
 
@@ -218,10 +163,10 @@ sudo tuned-adm profile balanced
 #sudo tuned-adm profile powersave
 
 # Virtual Machines
-sudo systemctl enable --now libvirtd
+# sudo systemctl enable --now libvirtd
 
 # Management of local/remote system(s) - available via http://localhost:9090
-sudo systemctl enable --now cockpit.socket
+# sudo systemctl enable --now cockpit.socket
 
 ###
 # Theming and GNOME Options
@@ -287,12 +232,12 @@ gsettings set org.gnome.nautilus.list-view use-tree-view true
 
 # Steam games (32bit) have issues with the too new 32bit compat libs in fedora
 # Flatpak is the better option here
-if [ ! -z "$STEAMFLAT" ]; then
-	sudo dnf install -y flatpak
-	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	flatpak -y install flathub com.valvesoftware.Steam
-	# Installed but not displayed? Check with: flatpak run com.valvesoftware.Steam
-fi
+#if [ ! -z "$STEAMFLAT" ]; then
+#	sudo dnf install -y flatpak
+#	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#	flatpak -y install flathub com.valvesoftware.Steam
+#	# Installed but not displayed? Check with: flatpak run com.valvesoftware.Steam
+#fi
 
 
 ###
@@ -300,8 +245,8 @@ fi
 # Reenabling them just to make sure.
 ###
 
-sudo sed -i '0,/enabled=0/s/enabled=0/enabled=1/g' /etc/yum.repos.d/fedora-updates-modular.repo
-sudo sed -i '0,/enabled=0/s/enabled=0/enabled=1/g' /etc/yum.repos.d/fedora-modular.repo
+#sudo sed -i '0,/enabled=0/s/enabled=0/enabled=1/g' /etc/yum.repos.d/fedora-updates-modular.repo
+#sudo sed -i '0,/enabled=0/s/enabled=0/enabled=1/g' /etc/yum.repos.d/fedora-modular.repo
 
 #The user needs to reboot to apply all changes.
 echo "Please Reboot" && exit 0
